559188c31dca575ebeaa1d0f1f18af6f40669b65
julian <julielmejor15@hotmail.com>
2022-09-13 19:29:32 -0500

    Actualizando el repo

---
+++ primerDesafio/index.js
@@ -0,0 +1,9 @@
+let producto = ""
+alert("Bienvenido creador de productos:");
+let productDeseo = prompt("Digite el número de productos que desea: ");
+
+for(let i = 1; i <= productDeseo; i++){
+    alert("Producto número: "+ i);
+}
+
+alert("Gracias por crear los productos, recarga la pagina para volver a intentarlo!")
---
+++ segundoDesafio/index.js
@@ -0,0 +1,21 @@
+
+alert("Bienvenido a simulador de Inventariado:");
+let opcionPrincipal = prompt("Puedes elegir: 1. INGRESO 2.RETIRO 3.CAMBIO");
+
+switch(opcionPrincipal){
+    case '1':
+        let objetoIngreso = prompt("Objeto a ingresar: ")
+        let numeroIngreso = prompt("Número de unidades del objeto/producto: ")
+        alert('Usted acaba de agregar ' + numeroIngreso +' unidades del producto u objeto ' + objetoIngreso)
+        break;
+    case '2':
+        let objetoRetiro = prompt("Objeto a ingresar: ")
+        let numeroRetiro = prompt("Número de unidades del objeto/producto: ")
+        alert('Usted acaba de retirar ' + numeroRetiro +' unidades del producto u objeto ' + objetoRetiro)
+        break;
+    case '3':
+        let objetoCambio1 = prompt("¿Qué objeto/producto desea cambiar?")
+        let objetoCambio2 = prompt("¿Por cual objeto/producto realizará el cambio?")
+        alert('Usted acaba de realizar un cambio de ' + objetoCambio1 + ' por ' + objetoCambio2 + '.... Gracias por realizar el cambio')
+}
+
